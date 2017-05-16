9f3e28de4e431ef461e6982250f41b92cd2493e2
Sam Houghton <sam@foundersfactory.co>
2017-05-16 18:01:08 +0100

    Initial commit

---
+++ Source/MagicRequestController.swift
@@ -0,0 +1,80 @@
+//
+//  FactoryRequestController.swift
+//
+//  Created by Sam Houghton on 16/05/2017.
+//
+//
+
+import Foundation
+
+public class MagicRequestController {
+    
+    let baseUrl: URL
+    
+    public init(baseUrl url: URL) {
+        
+        baseUrl = url
+    }
+    
+    public init(urlString: String) {
+        
+        baseUrl = URL(string: urlString)!
+    }
+    
+    public func get(urlpath path: String, completion: @escaping (Any?, URLResponse?, Error?) -> Swift.Void) {
+        
+        let defaultSessionConfiguration = URLSessionConfiguration.default
+        let defaultSession = URLSession(configuration: defaultSessionConfiguration)
+        
+        let urlRequest = URLRequest(url: baseUrl.appendingPathComponent(path))
+        
+        let dataTask = defaultSession.dataTask(with: urlRequest) { (data, response, error) in
+            
+            guard
+                let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                    
+                    completion(nil, response, error)
+                    
+                    return
+            }
+            
+            completion(json, response, error)
+        }
+        
+        dataTask.resume()
+    }
+    
+    public func post(urlpath path: String, body: [String: Any], completion: @escaping (Any?, URLResponse?, Error?) -> Swift.Void) {
+        
+        guard let data = try? JSONSerialization.data(withJSONObject: body, options: []) else {
+            
+            completion(nil, nil, nil)
+            return
+        }
+        
+        let defaultSessionConfiguration = URLSessionConfiguration.default
+        let defaultSession = URLSession(configuration: defaultSessionConfiguration)
+        
+        var request = URLRequest(url: baseUrl.appendingPathComponent(path))
+        request.httpBody = data
+        request.allHTTPHeaderFields = ["Content-Type":"application/json"]
+        request.httpMethod = "POST"
+        
+        let task = defaultSession.dataTask(with: request) { (data, response, error) in
+            
+            guard
+                let data = data,
+                let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                    
+                    completion(nil, response, error)
+                    
+                    return
+            }
+            
+            completion(json, response, error)
+        }
+        
+        task.resume()
+    }
+}
